24faf25f5821b99f7d884e9706e1cada92b81f3c
CacaceNicolas <147722690+CacaceNicolas@users.noreply.github.com>
2025-06-30 15:39:38 -0300

    Update app.routes.ts
    arregle el merge gagá dde nicolas cacace

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,20 +4,14 @@ import { Signup } from './signup/signup';
 import { RecuperarPassword } from './recuperar-password/recuperar-password';
 import { EnviarCorreoPassword } from './enviar-correo-password/enviar-correo-password';
 import { Ingredientes } from './ingredientes/ingredientes';
-
 import { AuthGuard } from './auth-guard';
-=======
-import { PaginaPrincipalComponent } from './paginaPrincipal/pagina-principal';
-
+import { PaginaPrincipalComponent } from './paginaPrincipal/pagina-principal'; 
 
 export const routes: Routes = [
-
-    {path : "logni", component: Login},
-    {path : "singup", component: Signup},
-    {path : "recuperarPassword/:token", component : RecuperarPassword},
-    {path : "enviarCorreoPassword", component : EnviarCorreoPassword},
-
-    {path : "ingredientes", component : Ingredientes, canActivate: [AuthGuard]} 
-=======
-    
+  { path : "", component: PaginaPrincipalComponent }, 
+  { path : "login", component: Logni}, 
+  { path : "signup", component: Signup},
+  { path : "recuperarPassword/:token", component : RecuperarPassword},
+  { path : "enviarCorreoPassword", component : EnviarCorreoPassword},
+  { path : "ingredientes", component : Ingredientes, canActivate: [AuthGuard]},
 ];
